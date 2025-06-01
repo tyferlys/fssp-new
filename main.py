1f7a6512a2faa4d8c398072130755a8df257f319
Denis Klimov <tyferly2003@gmail.com>
2025-06-01 16:50:12 +0400

    смена драйвера

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import loguru
 import uvicorn
 from fastapi import FastAPI
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from starlette.middleware.cors import CORSMiddleware
 
 from src.task.router import router as task_router
@@ -20,6 +21,8 @@ app.include_router(task_router)
 
 if __name__ == "__main__":
     loguru.logger.info("драйвер запуск")
-    driver = webdriver.Chrome()
+
+    options = Options()
+    driver = webdriver.Chrome(options=options)
     loguru.logger.info("драйвер получен")
     uvicorn.run(app, host="0.0.0.0", port=9004)
