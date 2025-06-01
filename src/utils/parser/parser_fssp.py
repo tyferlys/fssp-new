0a590a616f026988c8bb49ae1c211df1711453be
Denis Klimov <tyferly2003@gmail.com>
2025-06-01 14:24:23 +0400

    смена драйвера

--- src/utils/parser/parser_fssp.py
+++ src/utils/parser/parser_fssp.py
@@ -9,7 +9,7 @@ import requests
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from seleniumwire import webdriver
+from selenium import webdriver
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
