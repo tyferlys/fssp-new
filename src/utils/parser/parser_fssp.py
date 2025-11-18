5b034f165107873544a3861abc735b0c4d121479
tyfery <tyferly2003@gmail.com>
2025-11-18 14:03:09 +0400

    полный фикс

--- src/utils/parser/parser_fssp.py
+++ src/utils/parser/parser_fssp.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import os
 import re
 import shutil
@@ -8,6 +9,8 @@ import uuid
 
 import loguru
 import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 
 from selenium.webdriver.common.by import By
@@ -17,230 +20,187 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-from src.task.schemas import InputTask
+from src.task.schemas import InputTask, OutputTask
 from src.utils.CaptchaManager import CaptchaManager
 
 
 class ParserFSSP:
-    _url = "https://fssp.gov.ru/iss/ip"
+    _url = "https://fssp.gov.ru/iss/ip/?is%5Bvariant%5D=2"
     _current_directory = os.path.dirname(os.path.abspath(__file__))
-    _headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1"
-    }
 
     @classmethod
-    def _enter_data(cls, driver, actions, input_task: InputTask):
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "b-form__radio"))
-        )
-        driver.find_elements(By.CLASS_NAME, "b-form__radio")[0].click()
-        loguru.logger.info("Выбираем данные для поиска")
-
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "chosen-search-input"))
-        )
-        driver.find_elements(By.CLASS_NAME, "chosen-search-input")[0].click()
-
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//li[contains(@class, 'active-result') and text()='Все регионы']"))
-        )
-        driver.find_elements(By.XPATH, "//li[contains(@class, 'active-result') and text()='Все регионы']")[0].click()
-
-        href = driver.find_elements(By.XPATH, "//a[contains(@href, 'https://www.gosuslugi.ru/')]")[0]
-        actions.move_to_element(href).perform()
-        time.sleep(2)
-
-        driver.find_elements(By.XPATH, "//input[contains(@name, 'is[last_name]')]")[0].send_keys(input_task.last_name)
-
-        driver.find_elements(By.XPATH, "//input[contains(@name, 'is[first_name]')]")[0].send_keys(input_task.first_name)
-
-        driver.find_elements(By.XPATH, "//input[contains(@name, 'is[patronymic]')]")[0].send_keys(input_task.middle_name)
-
-        driver.find_elements(By.XPATH, "//input[contains(@name, 'is[date]')]")[0].send_keys(input_task.birth_date)
-
-        loguru.logger.success("Все данные выбрали")
+    def _prepare_test_to_html(cls, text: str) -> str:
+        json_str = re.sub(r'^[^(]*\((.*)\)[^)]*$', r'\1', text)
+        data = json.loads(json_str)
+        return data["data"]
 
     @classmethod
-    def _send_request(cls, driver) -> bool:
-        WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'btn-sbm')]"))
-        )
-        driver.find_elements(By.XPATH, "//input[contains(@id, 'btn-sbm')]")[0].click()
+    def _get_result(cls, input_task: InputTask):
+        params1 = {
+            "callback": "jQuery37105573464652258195_1763455204114",
+            "system": "ip",
+            "is[extended]": "1",
+            "nocache": "1",
+            "is[variant]": "1",
+            "is[region_id][0]": "-1",
+            "is[last_name]": input_task.last_name,
+            "is[first_name]": input_task.first_name,
+            "is[drtr_name]": "",
+            "is[ip_number]": "",
+            "is[patronymic]": input_task.middle_name,
+            "is[date]": input_task.birth_date,
+            "is[address]": "",
+            "is[id_number]": "",
+            "is[id_type][0]": "",
+            "is[id_issuer]": "",
+            "is[inn]": "",
+            "_": "1763455204124"
+        }
+        headers1 = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1"
+        }
 
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.ID, "ncapcha-submit"))
-            )
-            return True
-        except Exception as e:
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'b-search-message__text') or contains(@class, 'results')]"))
-            )
-            if driver.find_element(By.XPATH, "//div[contains(@class, 'b-search-message__text') or contains(@class, 'results')]"):
-                text_element = driver.find_element(By.XPATH, "//div[contains(@class, 'b-search-message__text') or contains(@class, 'results')]")
-                loguru.logger.warning(text_element.text.strip())
-                return False
+        response1 = requests.get("https://is-go.fssp.gov.ru/ajax_search", params=params1, headers=headers1)
+        soup1 = BeautifulSoup(cls._prepare_test_to_html(response1.text), 'html.parser')
 
-    @classmethod
-    def _get_answer(cls, driver):
-        loguru.logger.info("Отправили запрос на получение данных")
+        captcha_base64 = soup1.find('img').get('src')
+        code_id = soup1.find('form').get('url').split('code_id=')[1].split('&')[0]
 
-        for i in range(0, 5):
-            WebDriverWait(driver, 30).until(
-                EC.element_to_be_clickable((By.ID, "ncapcha-submit"))
-            )
-            time.sleep(2)
+        captcha = CaptchaManager.get_answer_captcha(captcha_base64)
+        loguru.logger.success(f"Каптча распознана - {captcha}")
 
-            # loguru.logger.info(driver.page_source)
-            try:
-                captcha_base64 = driver.find_element(By.XPATH, "//img[contains(@id, 'capchaVisualImage')]").get_attribute("src")
-            except:
-                captcha_base64 = driver.find_element(By.XPATH,"//img[contains(@id, 'capchaVisual')]").get_attribute("src")
-            captcha = CaptchaManager.get_answer_captcha(captcha_base64)
+        params2 = {
+            "callback": "jQuery37105573464652258195_1763455204114",
+            "is[variant]": "1",
+            "is[ip_number]": "",
+            "is[date]": input_task.birth_date,
+            "is[id_type][0]": "",
+            "is[inn]": "(empty)",
+            "is[region_id][0]": "-1",
+            "is[last_name]": input_task.last_name,
+            "is[drtr_name]": "",
+            "is[address]": "",
+            "is[id_issuer]": "",
+            "is[extended]": "1",
+            "nocache": "1",
+            "is[id_number]": "",
+            "system": "ip",
+            "is[first_name]": input_task.first_name,
+            "is[patronymic]": input_task.middle_name,
+            "code_id": code_id,
+            "code": captcha,
+            "_": "1763455204125"
+        }
+        headers2 = {
+            "accept": "*/*",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cookie": "_ym_uid=1760168378506978246; _ym_d=1760168378; _ym_isad=2",
+            "referer": "https://fssp.gov.ru/",
+            "sec-ch-ua": "\"Chromium\";v=\"142\", \"Google Chrome\";v=\"142\", \"Not_A Brand\";v=\"99\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "script",
+            "sec-fetch-mode": "no-cors",
+            "sec-fetch-site": "same-site",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+        }
 
-            loguru.logger.success(f"Каптча распознана - {captcha}")
-            driver.find_elements(By.XPATH, "//input[contains(@id, 'captcha-popup-code')]")[0].send_keys(captcha)
-            driver.find_elements(By.XPATH, "//input[contains(@id, 'ncapcha-submit')]")[0].click()
+        response2 = requests.get("https://is-go.fssp.gov.ru/ajax_search", params=params2, headers=headers2)
+        soup2 = BeautifulSoup(cls._prepare_test_to_html(response2.text), 'html.parser')
 
-            try:
-                WebDriverWait(driver, 120).until(
-                    EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'b-search-message__text') or contains(@class, 'results-frame') or contains(@class, 'b-form__label--error')]"))
-                )
-
-                if len(driver.find_elements(By.CSS_SELECTOR, ".b-form__label--error")) > 0:
-                    loguru.logger.warning(driver.find_elements(By.CSS_SELECTOR, ".b-form__label--error")[0].text.strip().lower())
-                    continue
-                else:
-                    break
-            except Exception as e:
-                break
-
-    @classmethod
-    def _get_result(cls, driver):
-        WebDriverWait(driver, 60).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'b-search-message__text') or contains(@class, 'results-frame')]"))
-        )
-        text_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'b-search-message__text')]")
-        text_element = ""
-        for text_element_temp in text_elements:
-            text_element += text_element_temp.text.strip().lower() + " "
-
-        if "по вашему запросу ничего не найдено" in text_element:
+        if "по вашему запросу ничего не найдено" in soup2.text.lower():
             return True, "Ничего не найдено"
+
+        results_frame = soup2.find('div', class_='results-frame')
+        if not results_frame:
+            return False, "Таблица не найдена"
+
+        rows = soup2.find_all('td', attrs={'colspan': False})
+
+        if len(rows) % 8 == 0:
+            loguru.logger.success("Количество блоков верное")
         else:
-            results = {}
+            loguru.logger.warning("Количество блоков неверное")
+            return False, "Ошибка в количестве блоков"
 
-            rows = [row for row in driver.find_element(By.CLASS_NAME, 'results-frame').find_elements(By.TAG_NAME, 'tr') if "region-title" not in row.get_attribute("class")]
+        results = {}
+        index_start = 0
+        index_block = 0
+        while index_start + 8 <= len(rows):
+            person = re.sub(r'\s+', ' ', rows[index_start + 0].text.strip())
+            person_lines = [re.sub(r'\s+', ' ', line.strip()) for line in rows[index_start + 0].text.strip().split('\n')]
+            ip_num_lines = [re.sub(r'\s+', ' ', line.strip()) for line in rows[index_start + 1].text.strip().split('\n')]
+            requisites = re.sub(r'\s+', ' ', rows[index_start + 2].text.strip())
+            ip_end = re.sub(r'\s+', ' ', rows[index_start + 3].text.strip())
+            amount = re.sub(r'\s+', ' ', rows[index_start + 5].text.strip())
+            department = re.sub(r'\s+', ' ', rows[index_start + 6].text.strip())
+            bailiff = re.sub(r'\s+', ' ', rows[index_start + 7].text.strip())
+            amount_sum_search = re.search(r'\d+\.\d{2}', amount)
 
-            loguru.logger.info(f"Таблица нашлась, статус -  {text_element}, количество строк - {len(rows)}")
-            for i, row in enumerate(rows):
-                columns = [c.text.strip() for c in row.find_elements(By.TAG_NAME, 'td')]
-                if len(columns) == 0:
-                    continue
+            result = {
+                "Должник": person.strip(),
+                "ФИО": person_lines[0].strip(),
+                "Дата рождения": person_lines[1].strip(),
+                "Исполнительное производство": re.sub(r' от .+', '', ip_num_lines[0]),
+                "Дата начала производства": re.sub(r'.+ от ', '', ip_num_lines[0]),
+                "Реквизиты исполнительного документа": requisites,
+                "Дата, причина окончания": ip_end,
+                "Окончено": ip_end != "",
+                "Предмет исполнения, сумма непогашенной задолженности": amount,
+                "Отдел судебных приставов": department,
+                "Судебный пристав-исполнитель": bailiff
+            }
 
-                person = columns[0].replace('\n', ' ').strip()
-                person_lines = columns[0].split('\n')
-                ip_num_lines = columns[1].split('\n')
-                requisites = columns[2].replace('\n', ' ')
-                ip_end = columns[3].replace('\n', ' ')
-                amount = columns[5].replace('\n', ' ')
-                department = columns[6].replace('\n', ' ')
-                bailiff = columns[7].replace('\n', ' ')
-                amount_sum_search = re.search(r'\d+\.\d{2}', amount)
+            if amount_sum_search:
+                result["Сумма задолженности"] = amount_sum_search.group(0)
 
-                result = {
-                    "Должник": person,
-                    "ФИО": person_lines[0].strip(),
-                    "Дата рождения": person_lines[1].strip(),
-                    "Исполнительное производство": re.sub(r' от .+', '', ip_num_lines[0]),
-                    "Дата начала производства": re.sub(r'.+ от ', '', ip_num_lines[0]),
-                    "Реквизиты исполнительного документа": requisites,
-                    "Дата, причина окончания": ip_end,
-                    "Окончено": ip_end != "",
-                    "Предмет исполнения, сумма непогашенной задолженности": amount,
-                    "Отдел судебных приставов": department,
-                    "Судебный пристав-исполнитель": bailiff
-                }
+            if len(person_lines) > 2:
+                result["Место рождения"] = person_lines[2].strip()
 
-                if amount_sum_search:
-                    result["Сумма задолженности"] = amount_sum_search.group(0)
+            if len(ip_num_lines) > 1:
+                result["Свободное исполнительное производство"] = ip_num_lines[1].strip()
 
-                if len(person_lines) > 2:
-                    result["Место рождения"] = person_lines[2].strip()
+            if ip_end:
+                ip_date_end = re.search(r'\d{2}\.\d{2}\.\d{4}', ip_end)
+                if ip_date_end:
+                    result["Дата окончания"] = ip_date_end.group()
 
-                if len(ip_num_lines) > 1:
-                    result["Свободное исполнительное производство"] = ip_num_lines[1].strip()
+                result["Причина окончания"] = re.sub(r'\d{2}\.\d{2}\.\d{4}', '', ip_end).strip()
 
-                if ip_end:
-                    ip_date_end = re.search(r'\d{2}\.\d{2}\.\d{4}', ip_end)
-                    if ip_date_end:
-                        result["Дата окончания"] = ip_date_end.group()
+            results[index_block] = result
+            index_block += 1
+            index_start += 8
 
-                    result["Причина окончания"] = re.sub(r'\d{2}\.\d{2}\.\d{4}', '', ip_end).strip()
-
-                results[i] = result
-            return True, results
+        return True, results
 
     @classmethod
     def start_parse(cls, input_task: InputTask):
-        # proxy_string = await get_proxy()
-        loguru.logger.info("Получаем driver для fssp")
-
-        temp_user_data_dir = tempfile.mkdtemp(prefix="chrome_", suffix="_profile")
-        options = Options()
-        # options.binary_location = "/usr/bin/chromium"  # путь к Chromium внутри контейнера
-        # options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-
-        driver = webdriver.Chrome(options=options)
-
         loguru.logger.info(f"Старт работы парсера - {input_task}")
         try:
-            actions = ActionChains(driver)
-            driver.get(cls._url)
-
-            cls._enter_data(driver, actions, input_task)
-
-            count = 1
-            while not cls._send_request(driver):
-                if count >= 5:
-                    raise Exception("Запрос не отправляется. Попробуйте позже")
-                driver.execute_script("location.reload();")
-                time.sleep(5)
-                cls._enter_data(driver, actions, input_task)
-                count += 1
-
-            cls._get_answer(driver)
-
-            status, result = cls._get_result(driver)
+            result = cls._get_result(input_task)
             loguru.logger.success(result)
             return result
         except Exception as e:
             loguru.logger.exception(e)
             raise Exception()
         finally:
-            shutil.rmtree(temp_user_data_dir)
-            time.sleep(1)
-            driver.quit()
-            time.sleep(1)
+            pass
 
 
 if __name__ == '__main__':
-    for i in range(0, 100):
-        loguru.logger.info("---------------------------------------------------------------------------------------")
-        ParserFSSP.start_parse(
-            InputTask(
-                last_name="Черноглазов",
-                first_name="Владислав",
-                middle_name="Сергеевич",
-                birth_date="22.04.1989"
-            )
+    ParserFSSP.start_parse(
+        InputTask(
+            last_name="Черноглазов",
+            first_name="Владислав",
+            middle_name="Сергеевич",
+            birth_date="22.04.1989"
         )
+    )
 
 
